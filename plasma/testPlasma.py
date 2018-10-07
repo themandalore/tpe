76e472974c06cf31e7f852d0b899a52b4daf7e26
themandalore <fett.nicholas@yahoo.com>
2018-10-06 22:03:42 -0400

    tests

--- plasma/testPlasma.py
+++ plasma/testPlasma.py
@@ -10,7 +10,7 @@ from Naked.toolshed.shell import execute_js, muterun_js, run_js
 # from .utils import confirm_tx, get_deposit_hash
 # from .transactions import encode_utxo_id, decode_utxo_id
 # from .constants import NULL_ADDRESS, NULL_ADDRESS_HEX
-
+NULL_ADDRESS = NULL_BYTE * 20
 
 node_url ="http://localhost:8545" #https://rinkeby.infura.io/
 net_id = 60 #eth network ID
@@ -19,11 +19,21 @@ contract_address = ""
 public_keys = ["0xe010ac6e0248790e08f42d5f697160dedf97e024","0xcdd8fa31af8475574b8909f135d510579a8087d3","0xb9dd5afd86547df817da2d0fb89334a6f8edd891","0x230570cd052f40e14c14a81038c6f3aa685d712b","0x3233afa02644ccd048587f8ba6e99b3c00a34dcc"]
 private_keys = ["3a10b4bc1258e8bfefb95b498fb8c0f0cd6964a811eabca87df5630bcacd7216","d32132133e03be292495035cf32e0e2ce0227728ff7ec4ef5d47ec95097ceeed","d13dc98a245bd29193d5b41203a1d3a4ae564257d60e00d6f68d120ef6b796c5","4beaa6653cdcacc36e3c400ce286f2aefd59e2642c2f7f29804708a434dd7dbe","78c1c7e40057ea22a36a0185380ce04ba4f333919d1c5e2effaf0ae8d6431f14"]
 
+
+def get_deposit_hash(owner, token, value):
+    return u.sha3(owner + token + b'\x00' * 31 + u.int_to_bytes(value))\
+
 def createSignature():
-	_utxoPos =
-	_txBytes =
-	_proof =
-	_sigs =
+	dep_blknum = getDepositBlock()
+	tx1 = Transaction(0, 0, 0, 0, 0, 0,
+                      NULL_ADDRESS,
+                      public_keys[1], 1e18, NULL_ADDRESS, 0)
+	_utxoPos = encode_utxo_id(dep_blknum, 0, 0)
+	_txBytes = get_deposit_hash(public_keys[1], NULL_ADDRESS, 1e18)
+	 merkle = FixedMerkle(16, [_txBytes], True)
+	_proof = merkle.create_membership_proof(_txBytes)
+	confirmSig1 = confirm_tx(tx1, root_chain.getPlasmaBlock(dep_blknum)[0], private_keys[1])
+	_sigs = tx1.sig1 + tx1.sig2 + confirmSig1
 	return _utxoPos, _txBytes, _proof, _sigs;
 
 def startExit():
@@ -32,7 +42,8 @@ def startExit():
 	root,timestamp = getPlasmaBlock();
 	print(root,timestamp)
 	while True:
-		_utxoPos, _txBytes, _proof, _sigs = createSignature(block)
+		_utxoPos, _txBytes, _proof, _sigs = createSignature()
+		break
 		arg_string =""+ str(_utxoPos) + " "+str(_txBytes)+" "+str(_proof)+" "+str(_sigs)+" "+str(contract_address)+" "+str(public_keys[miners_started])+" "+str(private_keys[miners_started])
 		run_js('submitter.js',arg_string);
 		console.log("Withdrawal Submitted");
@@ -56,10 +67,18 @@ def getPlasmaBlock():
 	r = requests.post(node_url, data=json.dumps(payload));
 	val = jsonParser(r)
 	print(val)
-	root = int(val['result'][2:34])
+	root = val['result'][0:34]
 	timestamp = int(val['result'][34:])
 	return root,timestamp;
 
+def getDepositBlock():
+	print (contract_address)
+	payload = {"jsonrpc":"2.0","id":net_id,"method":"eth_call","params":[{"to":contract_address,"data":"0xbcd59261"}, "latest"]}
+	r = requests.post(node_url, data=json.dumps(payload));
+	val = jsonParser(r)
+	print(val)
+	deposit = int(val['result'][2:34])
+	return deposit;
 
 def getAddress():
 	global last_block
